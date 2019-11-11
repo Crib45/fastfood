d40271e48d5ec74b117171a381d8d26920b79e4b
Crib45 <stefanmilojevic45@gmail.com>
2019-11-11 12:38:16 +0100

    Config updated for DB

--- src/main/java/com/fastfoodsm/fastfood/config/Config.java
+++ src/main/java/com/fastfoodsm/fastfood/config/Config.java
@@ -1,10 +1,22 @@
 package com.fastfoodsm.fastfood.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {"com.fastfoodsm.fastfood.repository"},
@@ -14,4 +26,39 @@ public class Config {
     @Autowired
     Environment env;
 
+    @Primary
+    @Bean
+    public LocalContainerEntityManagerFactoryBean db1EntityManager() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dbDataSource());
+        em.setJpaProperties(additionalJpaProperties());
+        em.setPackagesToScan("rs.ac.metropolitan.isum.database.newisum.entity");
+        em.setPersistenceUnitName("db1EntityManager");
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        return em;
+    }
+
+    @Primary
+    @Bean
+    @ConfigurationProperties("db.datasource")
+    public DataSource dbDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+    private Properties additionalJpaProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        return properties;
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager dbTransactionManager() {
+
+        JpaTransactionManager transactionManager
+                = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(
+                db1EntityManager().getObject());
+        return transactionManager;
+    }
 }
